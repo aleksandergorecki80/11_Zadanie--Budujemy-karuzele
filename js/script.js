cbe766eed075732839f3deaec90132ed46447e32
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-04-27 09:38:32 +0100

    Add styles for dots

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// This get all DOM element after it's loaded
-
 $(function(){
 	console.log('DOM loaded - you can have fun');	//this code will execute after the DOM is loaded
 	var carouselList = $('#carousel ul');
@@ -19,89 +17,67 @@ $(function(){
 		fillDots(licznik);
 	}
 
-function moveSlidesBack (speed){
-	carouselList.animate({'marginLeft': 500}, speed, moveLastSlide);
-	licznik--;	
-}
-
-function fillDots (nrSlajd){
-	$('#menu li').css('border', '1px solid white').css('background-color', 'none');
-	$('#menu').find("li").eq(nrSlajd).css('background-color', 'white');
-	var puste = $('#menu li').not(nrSlajd).css('border', '1px solid white').css('background-color', 'none');
-console.log(licznik + 'poza funkcją');
-}
-
-	function moveFirstSlide(){
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
-		lastItem.after(firstItem);
-		carouselList.css({'margin-left': 0})
-	};
-
-	function moveLastSlide(){
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
-		firstItem.before(lastItem);
-		carouselList.css({'margin-left': 0})
-	};
-
-
-/*
-function getIndexOfLi (dotIndex) {
-
-	var firstItem = carouselList.find('li:first');
-	//var liItemsList = $('#carousel ul li');
-	var liItemsList = carouselList.find('li');
-
-	var newfirstItem = liItemsList.get(dotIndex);
-	firstItem.before(newfirstItem);
-	carouselList.css({'margin-left': 0})
-}
-*/
-
-
-function getIndexOfLi (dotIndex) {
-
-	var firstItem = carouselList.find('li:first');
-	//var liItemsList = $('#carousel ul li');
-	var liItemsList = carouselList.find('li');
-
-	var newfirstItem = liItemsList.get(dotIndex);
-	firstItem.before(newfirstItem);
-	carouselList.css({'margin-left': 0})
-}
-
-
-
-
-
-
-var dot = $('#menu li');
-
-dot.click(function() {
-	clearInterval(myTimer);
-  var index = dot.index( this );
-  //var dotNr = dot.eq(index);
-fillDots(index);
-
-
-console.log(licznik + ' = licznik');
-console.log(index + ' = index');
-
-if (licznik<index) {
-	for (i=licznik; i<index; i++){
-		changeSlide(300);
+	function moveSlidesBack (speed){
+		carouselList.animate({'marginLeft': 500}, speed, moveLastSlide);
+		licznik--;	
 	}
-} else if (licznik>index) {
-	for (i=licznik; i>index; i--){
-		moveSlidesBack(300);
+
+
+	function fillDots (nrSlajd){
+		$('#menu li').removeClass('active');
+		$('#menu li').eq(nrSlajd).addClass('active');
+
+		//$('#menu li').css('border', '1px solid white').css('background-color', 'none');
+		//$('#menu').find("li").eq(nrSlajd).css('background-color', 'white');
+		//$("#menu li:not(#p)").css("background-color", "yellow");
 	}
-}
-myTimer = setInterval(changeSlide, 3000);
-  //getIndexOfLi(index);
-});
+
+		function moveFirstSlide(){
+			var firstItem = carouselList.find('li:first');
+			var lastItem = carouselList.find('li:last');
+			lastItem.after(firstItem);
+			carouselList.css({'margin-left': 0})
+		};
+
+		function moveLastSlide(){
+			var firstItem = carouselList.find('li:first');
+			var lastItem = carouselList.find('li:last');
+			firstItem.before(lastItem);
+			carouselList.css({'margin-left': 0})
+		};
 
 
+	function getIndexOfLi (dotIndex) {
+
+		var firstItem = carouselList.find('li:first');
+		//var liItemsList = $('#carousel ul li');
+		var liItemsList = carouselList.find('li');
+
+		var newfirstItem = liItemsList.get(dotIndex);
+		firstItem.before(newfirstItem);
+		carouselList.css({'margin-left': 0})
+	}
+
+
+	$li = $('#menu li');
+
+	$li.click(function() {
+		clearInterval(myTimer);
+	  	var index = $li.index( this );
+		fillDots(index);
+
+		if (licznik<index) {
+			for (i=licznik; i<index; i++){
+				changeSlide(300);
+			}
+		} else if (licznik>index) {
+			for (i=licznik; i>index; i--){
+				moveSlidesBack(300);
+			}
+		}
+		myTimer = setInterval(changeSlide, 3000);
+	  
+	});
 
 });
 
