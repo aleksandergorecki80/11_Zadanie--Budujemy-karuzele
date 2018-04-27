d354909af8ec77a36f9459d85112965d78a55630
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-04-27 21:10:54 +0100

    Add animation forward and back

--- js/script.js
+++ js/script.js
@@ -1,94 +1,77 @@
 $(function(){
-	console.log('DOM loaded - you can have fun');	//this code will execute after the DOM is loaded
+	
 	var carouselList = $('#carousel ul');
 	var licznik = 0;
-	fillDots (licznik);
+	fillDots (licznik);		// set a first dot as active
 
 	var myTimer = setInterval(changeSlide, 3000);
+	var length = $('#menu li').length;
 
-
+	function carouselGoesLeft (){
+		var myTimer = setInterval(changeSlide, 3000);
+	};
 
 	function changeSlide(speed){	
 		licznik ++;
 		carouselList.animate({'marginLeft': -500}, speed, moveFirstSlide);
-		if (licznik==5){
+		if (licznik==length){
 			licznik=0;
 		}
-		fillDots(licznik);
-	}
+		fillDots(licznik);		//	change the active dot
+	};
 
 	function moveSlidesBack (speed){
 		carouselList.animate({'marginLeft': 500}, speed, moveLastSlide);
 		if (licznik==0){
-			licznik=5;
+			licznik=length;
 		}
 		licznik--;	
-		
-		fillDots(licznik);
-		console.log(licznik + ' 	- licznik w funkcji moveSlidesBack')
-	}
+		fillDots(licznik);		//	change the active dot
+	};
 
 
 	function fillDots (nrSlajd){
 		$('#menu li').removeClass('active');
 		$('#menu li').eq(nrSlajd).addClass('active');
+	};
 
-		//$('#menu li').css('border', '1px solid white').css('background-color', 'none');
-		//$('#menu').find("li").eq(nrSlajd).css('background-color', 'white');
-		//$("#menu li:not(#p)").css("background-color", "yellow");
-	}
-
-		function moveFirstSlide(){
-			var firstItem = carouselList.find('li:first');
-			var lastItem = carouselList.find('li:last');
-			lastItem.after(firstItem);
-			carouselList.css({'margin-left': 0})
-		};
-
-		function moveLastSlide(){
-			var firstItem = carouselList.find('li:first');
-			var lastItem = carouselList.find('li:last');
-			firstItem.before(lastItem);
-			carouselList.css({'margin-left': 0})
-		};
-
-
-	function getIndexOfLi (dotIndex) {
-
+	function moveFirstSlide(){
 		var firstItem = carouselList.find('li:first');
-		//var liItemsList = $('#carousel ul li');
-		var liItemsList = carouselList.find('li');
-
-		var newfirstItem = liItemsList.get(dotIndex);
-		firstItem.before(newfirstItem);
+		var lastItem = carouselList.find('li:last');
+		lastItem.after(firstItem);
 		carouselList.css({'margin-left': 0})
-	}
+	};
 
+	function moveLastSlide(){
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+		firstItem.before(lastItem);
+		carouselList.css({'margin-left': 0})
+	};
 
 	$li = $('#menu li');
-
 	$li.click(function() {
 		clearInterval(myTimer);
 	  	var index = $li.index( this );
+		
 		fillDots(index);
 
 		if (licznik<index) {
 			for (i=licznik; i<index; i++){
-				changeSlide(300);
+				changeSlide(300);			// checks a clicked dot and moves carousel left
 			}
 		} else if (licznik>index) {
 			for (i=licznik; i>index; i--){
-				moveSlidesBack(300);
+				moveSlidesBack(300);		// checks a clicked dot and moves carousel right
 			}
 		}
 		myTimer = setInterval(changeSlide, 3000);
-	  
 	});
 
 	$('.fa-chevron-circle-left').click(function(){
 		clearInterval(myTimer);
 		moveSlidesBack(300);
-		myTimer = setInterval(changeSlide, 3000);
+		myTimer = setInterval(moveSlidesBack, 3000);
 	});
 
 	$('.fa-chevron-circle-right').click(function(){
@@ -96,6 +79,7 @@ $(function(){
 		changeSlide(300);	
 		myTimer = setInterval(changeSlide, 3000);	
 	});
+
 
 });
 
